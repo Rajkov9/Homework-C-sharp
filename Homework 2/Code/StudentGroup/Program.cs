4910d5e8f4993165b7f0f2127de4b34d741ba43a
Rajkov9 <rajkov.ivan24@gmail.com>
2025-03-19 20:36:39 +0100

    new upload

---
+++ Homework 2/Code/StudentGroup/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] studentsG1 = new string[] {"Zdravko", "Petko", "Stanko", "Branko", "Trajko"} ;
+string[] studentsG2 = new string[] { "Sinisha", "Jovanche", "Aleksandra", "Ivan", "Ljubisha" };
+int option = GetStudentGroup();
+
+if (option == 1)
+{
+    foreach (var s in studentsG1)
+    {
+        Console.WriteLine(s);
+    }
+}
+else
+{
+    foreach (var s2 in studentsG2)
+    {
+        Console.WriteLine(s2);
+    }
+}
+    static int GetStudentGroup()
+    {
+        while (true)
+        {
+            Console.WriteLine("Please select option 1 or option 2 to select a student group");
+            if (int.TryParse(Console.ReadLine(), out int option) && (option == 1 || option == 2))
+            {
+                return option;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option.Please try again.");
+            }
+        }
+    }
---
+++ Homework 2/Code/SumOfEven/Program.cs
@@ -0,0 +1,24 @@
+﻿static void SumOfEven()
+{
+    int[] array = new int [6];
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        while (true)
+        {
+            Console.WriteLine("Please enter a number so that it may be added to the array.");
+            if(int.TryParse(Console.ReadLine(), out array[i]))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.Please enetr a valid number");
+            }
+        }
+        sum += array[i];
+    }
+    Console.WriteLine("The total sum is: " + sum); //the result in the test output is wrong
+}
+
+SumOfEven();
