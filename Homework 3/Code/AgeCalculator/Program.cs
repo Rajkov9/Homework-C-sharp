a5e9eadbfee42f9fa0601cd8ad7ca71cc539572d
Rajkov9 <rajkov.ivan24@gmail.com>
2025-03-21 20:05:38 +0100

    new

---
+++ Homework 3/Code/AgeCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿static DateTime EnterDate()
+{
+    DateTime birthDate;
+    while (true)
+    {
+        Console.WriteLine("Please enter your full birthdate");
+        if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please try again.");
+        }
+    }
+    return birthDate;
+
+}
+static int CalculateAge(DateTime birthDate)
+{
+    DateTime today = DateTime.Today;
+    int age = today.Year - birthDate.Year;
+    if (birthDate > today.AddYears(-age))
+    {
+        age--;
+    }
+    return age;
+}
+DateTime birthDate = EnterDate();
+Console.WriteLine(CalculateAge(birthDate));
